b10aba50668bb567584c2d81d776906ccf8e4f4f
Kaiho Chan <kaihochan@kaihos-imac.lan>
2018-06-27 21:29:04 -0700

    fix: css style and Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,7 +13,7 @@ module.exports = function (grunt) {
 
   // Time how long tasks take. Can help when optimizing build times
   require('time-grunt')(grunt);
-
+  require('load-grunt-tasks')(grunt);
   // Automatically load required Grunt tasks
   require('jit-grunt')(grunt, {
     useminPrepare: 'grunt-usemin',
