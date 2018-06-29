1148d617e7271bd928edf911937a051a4171315a
Kaiho Chan <kaihochan@kaihos-imac.lan>
2018-06-28 17:42:00 -0700

    fix: Fix MyApp pages container height and test Service worker

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,7 +15,7 @@
  };
 
  if ('serviceWorker' in navigator) {
-     navigator.serviceWorker.register('/service_worker.js').then(function(registration) {
+     navigator.serviceWorker.register('https://kchan167.github.io/kaihochan/service_worker.js').then(function(registration) {
          //Registration was successful
          console.log('ServiceWorker registration successful with scope: ', registration.scope);
      }).catch(function(err) {
